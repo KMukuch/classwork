8bf95d836453ab806313fb71703d1157047fcc32
Mkrtich <mkuloian@gmail.com>
2022-11-22 18:55:17 +0300

    2022.11.22

---
+++ 2022.11.22/index.js
@@ -0,0 +1,33 @@
+//object
+const user={};
+
+user.name="Mkrtich";
+
+console.log(user);
+
+const smth={
+    name:"Blabla",
+    age:30,
+    "property shmropert":"Blabla"
+};
+
+console.log("name" in smth);
+console.log(smth);
+
+//for in loop
+for (let i in smth){
+    console.log(i,smth[i]);
+}
+
+//1
+const car1={};
+car1.brand="BMW";
+car1.year=2018;
+car1.price=20000;
+
+const car2={
+    brand:"Mercedes",
+    year:2018,
+    price:20000,
+};
+console.log(car1,car2);
